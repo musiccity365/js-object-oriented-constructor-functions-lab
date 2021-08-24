dddfee6f5256b68761f1f1cf181da34e1300eaee
musiccity365 <m_sackstein@yahoo.com>
2021-08-24 16:34:44 -0400

    Done.

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ function Driver(name, age, experience) {
     this.experience = experience;
 }
 
-function PickupLocation(address, city) { this.address = address;
-    this.city = city; }
+function PickupLocation(address, city) {
+    this.address = address;
+    this.city = city;
+}
